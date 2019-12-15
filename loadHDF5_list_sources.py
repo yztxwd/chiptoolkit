bc7e330baf27643fa3816ef92ca1de6d98cf46dc
yztxwd <yztxwd@gmail.com>
2019-12-15 18:20:19 +0800

    Add list source script
    List all sources of specie specified in the file

---
+++ loadHDF5_list_sources.py
@@ -0,0 +1,31 @@
+#!python3
+#-*-coding:utf-8-*-
+
+description = """loadHDF5_list_sources.py
+
+List sources contained in HDF5 file
+
+Example:
+    $ python loadHDF5_list_sources.py mouse cache.h5
+
+Dependencies:
+    h5py
+
+@Copyright 2019, Jianyu Yang, Southern Medical University
+"""
+
+import sys
+import h5py
+
+def main():
+    filename = sys.argv[2]
+    specie = sys.argv[1]
+
+    hdf5 = h5py.File(filename, 'a')
+    chrkey = hdf5['%s' %specie].keys()[0]
+    print("Sources of %s in %s" %(specie, filename))
+    for source in hdf5['%s/%s' %(specie, chrkey)].keys():
+        print("\t%s" % source)
+
+if __name__ == '__main__':
+    main()
